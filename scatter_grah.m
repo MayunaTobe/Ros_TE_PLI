cf40446c2b63c1ad40882ec0428251d5b19231be
MayunaTobe <ova6jx5738@gmail.com>
2021-09-07 12:55:22 +0900

    ディレクトリの整理

--- scatter_grah.m
+++ scatter_grah.m
@@ -8,7 +8,6 @@ figure
 scatter(rst(:,2),rst(:,3))
 xlabel('rho')
 ylabel('PLI')
-%title('tau') %変える
 saveas(gcf,append('rho-PLI_',figtitle,'.png')) %変える
 
 %% rho-TE
@@ -16,35 +15,28 @@ figure
 scatter(rst(:,2),rst(:,4),'x')
 hold on
 scatter(rst(:,2),rst(:,5),'o')
-%legend('TE12','TE21')
-legend('TE12','TE21','Location','southwest')
+legend('TE1→2','TE2→1','Location','southwest')
 xlabel('rho')
 ylabel('TE')
-%title('tau') %変える
 ylim([0 inf])
-%saveas(gcf,'rho-TE_d0.png') %変える
 saveas(gcf,append('rho-TE_',figtitle,'.png')) %変える
 hold off
 
-%% B-rho
+%% A-rho
 figure
 scatter(rst(:,1),rst(:,2))
 xlabel('A')
 ylabel('rho')
-%title('tau')
-%saveas(gcf,'B-rho_d0.png') %変える
 saveas(gcf,append('A-rho_',figtitle,'.png')) %変える
 
-%% B-PLI
+%% A-PLI
 figure
 scatter(rst(:,1),rst(:,3))
 xlabel('A')
 ylabel('PLI')
-%title('tau') %変える
-%saveas(gcf,'B-PLI_d0.png') %変える
 saveas(gcf,append('A-PLI_',figtitle,'.png')) %変える
 
-%% B-TE
+%% A-TE
 figure
 scatter(rst(:,1),rst(:,4),'x')
 hold on
@@ -52,9 +44,7 @@ scatter(rst(:,1),rst(:,5),'o')
 legend('TE12','TE21')
 xlabel('A')
 ylabel('TE')
-%title('tau')
 ylim([0 inf])
-%saveas(gcf,'B-TE_d0.png') %変える
 saveas(gcf,append('A-TE_',figtitle,'.png')) %変える
 
 
@@ -63,12 +53,9 @@ figure
 scatter(rst(:,3),rst(:,4),'x')
 hold on
 scatter(rst(:,3),rst(:,5),'o')
-legend('TE12','TE21','Location','southeast')
-%legend('TE12','TE21')
+legend('TE1→2','TE2→1','Location','southeast')
 xlabel('PLI')
 ylabel('TE')
-%title('tau')
-%saveas(gcf,'PLI-TE_d0.png') %変える
 saveas(gcf,append('PLI-TE_',figtitle,'.png')) %変える
 
 %% TE-PLI
@@ -76,11 +63,8 @@ figure
 scatter(rst(:,4),rst(:,3),'x')
 hold on
 scatter(rst(:,5),rst(:,3),'o')
-legend('TE12','TE21','Location','southeast')
-%legend('TE12','TE21')
+legend('TE1→2','TE2→1','Location','southeast')
 xlabel('TE')
 ylabel('PLI')
 xlim([0 inf])
-%title('tau')
-%saveas(gcf,'TE-PLI_d0.png') %変える
 saveas(gcf,append('TE-PLI_',figtitle,'.png')) %変える
