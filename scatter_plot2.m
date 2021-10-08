716c7a36af0bfea9faf23f83e8512f373efd4e79
MayunaTobe <ova6jx5738@gmail.com>
2021-10-08 14:45:32 +0900

    epoch-PLIのプロットを出すコードを追加

--- scatter_plot2.m
+++ scatter_plot2.m
@@ -1,5 +1,6 @@
 %散布図作成
 
+%% TE-PLI 散布図
 % データ整理
 te=(te_ns12+te_ns21)/2; % 足して2で割る
 te(te<0)=NaN;
@@ -10,8 +11,21 @@ pli_ns_sort=pli_ns(i_sort);
 scatter(te,pli_ns,'.')
 hold on
 
-
 % プロット部分（近似曲線）
-plot(te_sort,smooth(movmean(pli_ns_sort,5),0.1,'loess'))
+plot(te_sort,smooth(movmean(pli_ns_sort,5),0.1,'loess'),'LineWidth',2)
 xlabel('TE')
 ylabel('PLI')
+
+%% epoch-PLIのグラフ
+c = 1;
+for i=1:5
+    subplot(5,1,i)
+    plot(pli(c,:))
+    xlim([0 101]);
+    ylim([0 1.0]);
+    %xlabel('epoch');
+    ylabel('PLI');
+    title(['A=',num2str(i*0.02)]);
+    c=c+200;
+end
+ xlabel('epoch');
